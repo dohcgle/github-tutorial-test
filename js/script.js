e2414b11fcc550cd8bbf77c2ccee608596dafe1e
ulugbek <utn1002@gmail.com>
2023-02-16 09:42:30 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+
+
+
+// incr, decr
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(5%3);
+// console.log(5 * 5 === "25");
+// const isAge = true;
+// const isClose = false;
+// const isChecked = false
+// console.log(isAge || isClose && isChecked && !isChecked);
+// console.log(2 + 2*2 != 8);
+
+
+// console.log("Samar" + "Badriddinov");
+// console.log("Badriddinov" + 3);
+// console.log(6 + +"3");
+
+
+
+// const user = "Samar";
+// const job = "JavaScript developer"
+
+// console.log(`User name is ${user}, he is ${job}`);
+
+// const channel = prompt("What's you favourate youtuber?");
+// console.log(`https://youtube.com/${channel}`);
+
+// const currentPlace = prompt("Where are you from?", "Tashkent");
+// console.log(currentPlace);
+
+// let number = 5.6;
+// console.log(4/0); // Infinity
+// console.log("Samar" * 12 ); //NaN
+
+// const clientName = "Samar";
+// console.log(clientName);
+
+// const isMarried = false;
+
+// // console.log(clientAge);
+
+// let und;
+// console.log(und);
+
+// const thief = {
+//     age: 30,
+//     jacket: "black",
+//     isLong: false
+// };
+
+// console.log(thief.jacket);
+// console.log(thief["isLong"]);
+
+// const colors = ["red", "black", "green", 10, [], {}]
+// console.log(colors[0]);
+
+// number = 10;
+// console.log(number);
+
+// let number = 10;
+// const borderColor = "red";
+
+// console.log(number);
+// console.log(borderColor);
+
+// const person = {
+//     name: "Samar",
+// }
+
+// person.name = 'Yusuf'
+// console.log(person);
